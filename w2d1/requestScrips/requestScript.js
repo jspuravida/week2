e535086c2de42039e8fa2c4e03b9162092b1e9c1
jspuravida <julian.salari@gmail.com>
2016-11-08 01:06:49 +0000

    requestScript

---
+++ w2d1/requestScrips/requestScript.js
@@ -0,0 +1,14 @@
+var request = require('request');
+var fs = require('fs');
+
+request.get('https://sytantris.github.io/http-examples/future.jpg')               // Note 1
+       .on('error', function (err) {                                   // Note 2
+         console.log(err.stack);
+       })
+       .on('response', function (response) {                           // Note 3
+         console.log('Response Status Code: ', response.statusCode);
+         console.log('Response Message : ', response.statusMessage);
+         console.log('Response Content Type: ', response.headers['content-type']);
+
+       })
+       .pipe(fs.createWriteStream('./future.jpg'));
