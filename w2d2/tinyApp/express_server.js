3fff4cdcdd830d8566e54fe3252d6d58d4f379e2
jspuravida <julian.salari@gmail.com>
2016-11-09 01:58:32 +0000

    Added url generator function, the url new page, and url show page, and json package

--- w2d2/tinyApp/express_server.js
+++ w2d2/tinyApp/express_server.js
@@ -1,6 +1,10 @@
 var express = require("express");
+
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -26,12 +30,39 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id,
                         fullURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString() {
+
+  var list = "abcdefghijklmnopqrstuvwxyz0123456789"; //list of characters to choose from
+  var urlShortened = ""; //stores the new url
+
+  for (var i = 0; i < 6; i++) { //loops until it hits the set max length
+    urlShortened += list.charAt(Math.floor(Math.random() * list.length)); //randomly outputs a character from the list and stores in urlShortened
+  }
+
+console.log("New and Shortened URL: " + urlShortened);
+
+}
+
+generateRandomString();
+
